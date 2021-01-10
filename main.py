693a96f97b60b7ded05b06f3bec5a1bca9a85f4c
vishalmainkanta <76800132+Vishal12328@users.noreply.github.com>
2021-01-10 18:19:46 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,18 @@
+
+def translator(phrase):
+    translation = ""
+    for letter in phrase:
+        if letter in "AEIOUaeiou":
+            translation = translation + "g"
+        else:
+            translation = translation + letter
+    return translation
+
+
+print(translator(input("enter a name: ")))
+
+
+
+
+
+
